0aa290db2267947f566a907b5a17e88a2b921755
Ruben Karlsson <neburgordon@gmail.com>
2020-10-13 07:56:45 +0200

    adding a test file and a script for the test

---
+++ tests/testing_clean.py
@@ -0,0 +1,12 @@
+from data.processing_verses import clean_verses as clean
+
+
+with open("f_tst.txt") as f:
+     lines  = f.read().split("\n")
+
+
+lines_clean = clean(lines)
+
+
+with open("/home/nebur/Desktop/poemautomator/tests/f_to_comp_w_f_tst.txt", "w") as f:
+    f.write("\n".join(lines_clean))
